a409238ce004ae267593fb83d682f98560709e61
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-07-25 16:56:28 +0100

    first commit

---
+++ Utilities.cs
@@ -0,0 +1,27 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2Stats {
+    public partial class CS2Stats {
+
+        // thanks to switz https://discord.com/channels/1160907911501991946/1160925208203493468/1170817201473855619
+        public int? GetCSTeamScore(CsTeam team) {
+            var teamManagers = Utilities.FindAllEntitiesByDesignerName<CCSTeam>("cs_team_manager");
+
+            foreach (var teamManager in teamManagers) {
+                if ((int)team == teamManager.TeamNum) {
+                    return teamManager.Score;
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
