1eeea76940a2461072159a72685d8651ceae20eb
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-08-06 18:52:26 +0100

    commit

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `Match` (
 
 CREATE TABLE IF NOT EXISTS `Player` (
     PlayerID bigint UNSIGNED,
-    Username varchar(32),
+	Username varchar(32),
     ELO int DEFAULT 1000,
     PRIMARY KEY (PlayerID)
 );
