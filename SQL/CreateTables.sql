cca729164b27d1f99d3066a615fa47fa60342b44
sarim <58489447+sarim-hk@users.noreply.github.com>
2025-04-03 01:40:48 +0100

    set livestatus attributes to be nullable

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -173,12 +173,12 @@ CREATE TABLE IF NOT EXISTS CS2S_LivePlayers (
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_LiveStatus (
-	StaticID int PRIMARY KEY NOT NULL,
-	MapID varchar(128) NOT NULL,
-    BombStatus int NOT NULL,
-    TScore int NOT NULL,
-    CTScore int NOT NULL,
-	InsertDate datetime DEFAULT CURRENT_TIMESTAMP NOT NULL
+	StaticID int PRIMARY KEY,
+	MapID varchar(128),
+    BombStatus int,
+    TScore int,
+    CTScore int,
+	InsertDate datetime DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Team_Players (
