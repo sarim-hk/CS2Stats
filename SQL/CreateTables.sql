f908716ecc6a6a2d03fba241227b0e3ab80a893e
unknown <demetriusgekas@hotmail.com>
2025-04-30 00:08:40 +0300

    clean up mysql schema

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -1,18 +1,18 @@
+-- This should probably just be stored in CS2S_Match
+-- since we don't store any additional info on maps, or reference this table anywhere else
 CREATE TABLE IF NOT EXISTS CS2S_Map (
     MapID varchar(128) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_PlayerInfo (
-    PlayerID bigint PRIMARY KEY NOT NULL,
+    PlayerID bigint unsigned PRIMARY KEY NOT NULL,
 	ELO int DEFAULT 1000 NOT NULL,
-    Username varchar(255) NOT NULL,
-    AvatarS varchar(255) DEFAULT "https://avatars.steamstatic.com/b5bd56c1aa4644a474a2e4972be27ef9e82e517e.jpg" NOT NULL,
-    AvatarM varchar(255) DEFAULT "https://avatars.steamstatic.com/b5bd56c1aa4644a474a2e4972be27ef9e82e517e_medium.jpg" NOT NULL,
-    AvatarL varchar(255) DEFAULT "https://avatars.steamstatic.com/b5bd56c1aa4644a474a2e4972be27ef9e82e517e_full.jpg" NOT NULL
+    Username varchar(64) NOT NULL,
+    AvatarHash varchar(64) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Match (
-    MatchID int PRIMARY KEY NOT NULL,
+    MatchID int unsigned PRIMARY KEY NOT NULL,
     MapID varchar(128) NOT NULL,
     StartTick int NOT NULL,
     EndTick int NOT NULL,
@@ -22,17 +22,20 @@ CREATE TABLE IF NOT EXISTS CS2S_Match (
 
 CREATE TABLE IF NOT EXISTS CS2S_Team (
     TeamID varchar(32) PRIMARY KEY,
-	Size int NOT NULL,
+	Size int unsigned NOT NULL,
     ELO int DEFAULT 1000 NOT NULL,
     Name varchar(64) DEFAULT "Team" NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_TeamResult (
     TeamID varchar(32) NOT NULL,
-	MatchID int NOT NULL,
-	Score int NOT NULL,
+	MatchID int unsigned NOT NULL,
+	Score smallint unsigned NOT NULL,
     Result ENUM("Win", "Loss", "Tie") NOT NULL,
-    Side int NOT NULL,
+    
+     -- t = 2, ct = 3
+    Side tinyint unsigned NOT NULL,
+
     DeltaELO int DEFAULT 0 NOT NULL,
     PRIMARY KEY (TeamID, MatchID),
     FOREIGN KEY (TeamID) REFERENCES CS2S_Team(TeamID),
@@ -40,34 +43,46 @@ CREATE TABLE IF NOT EXISTS CS2S_TeamResult (
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Round (
-    RoundID int PRIMARY KEY NOT NULL,
-    MatchID int NOT NULL,
+    RoundID int unsigned PRIMARY KEY,
+    MatchID int unsigned NOT NULL,
     WinnerTeamID varchar(32) NOT NULL,
     LoserTeamID varchar(32) NOT NULL,
-    WinnerSide int NOT NULL,
-	LoserSide int NOT NULL,
-    RoundEndReason int NOT NULL,
-    StartTick int NOT NULL,
-    EndTick int NOT NULL,
+
+    -- t = 2, ct = 3
+    WinnerSide tinyint unsigned NOT NULL,
+	LoserSide tinyint unsigned NOT NULL,
+
+    RoundEndReason tinyint unsigned NOT NULL,
+
+    StartTick int unsigned NOT NULL,
+    EndTick int unsigned NOT NULL,
+
     FOREIGN KEY (MatchID) REFERENCES CS2S_Match(MatchID),
     FOREIGN KEY (WinnerTeamID) REFERENCES CS2S_Team(TeamID),
     FOREIGN KEY (LoserTeamID) REFERENCES CS2S_Team(TeamID)
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Death (
-    DeathID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    RoundID int NOT NULL,
-    MatchID int NOT NULL,
-    AttackerID bigint NULL,
-    AttackerSide int NULL,
-    AssisterID bigint NULL,
-    AssisterSide int NULL,
-    VictimID bigint NOT NULL,
-	VictimSide int NOT NULL,
+    DeathID int unsigned PRIMARY KEY AUTO_INCREMENT,
+    RoundID int unsigned NOT NULL,
+    MatchID int unsigned NOT NULL,
+
+    AttackerID bigint unsigned NOT NULL,
+    VictimID bigint unsigned NOT NULL,
+    AssisterID bigint unsigned, 
+
+    -- t = 2, ct = 3
+    AttackerSide tinyint unsigned NOT NULL,
+	VictimSide tinyint unsigned NOT NULL,
+    AssisterSide tinyint unsigned,
+
+    -- generic = 0, head = 1, chest = 2, stomach = 3, leftarm = 4, rightarm = 5,
+    -- leftleg = 6, rightleg = 7, neck = 8, gear = 10
+    Hitgroup tinyint unsigned NOT NULL,
+
     Weapon varchar(32) NOT NULL,
-    Hitgroup int NOT NULL,
-    RoundTick int NOT NULL,
-    OpeningDeath bool NOT NULL,
+    RoundTick int unsigned NOT NULL,
+    OpeningDeath boolean NOT NULL,
     FOREIGN KEY (RoundID) REFERENCES CS2S_Round(RoundID),
 	FOREIGN KEY (MatchID) REFERENCES CS2S_Match(MatchID),
     FOREIGN KEY (AttackerID) REFERENCES CS2S_PlayerInfo(PlayerID),
@@ -76,17 +91,24 @@ CREATE TABLE IF NOT EXISTS CS2S_Death (
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Hurt (
-    HurtID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    RoundID int NOT NULL,
-    MatchID int NOT NULL,
-    AttackerID bigint NULL,
-    AttackerSide int NULL,
-    VictimID bigint NOT NULL,
-	VictimSide int NULL,
-    Damage int NOT NULL,
+    HurtID int unsigned PRIMARY KEY AUTO_INCREMENT,
+    RoundID int unsigned NOT NULL,
+    MatchID int unsigned NOT NULL,
+
+    AttackerID bigint unsigned NOT NULL,
+    VictimID bigint unsigned NOT NULL,
+
+    -- t = 2, ct = 3
+    AttackerSide tinyint unsigned NOT NULL,
+	VictimSide tinyint unsigned NOT NULL,
+
+    -- generic = 0, head = 1, chest = 2, stomach = 3, leftarm = 4, rightarm = 5,
+    -- leftleg = 6, rightleg = 7, neck = 8, gear = 10
+    Hitgroup tinyint unsigned NOT NULL,
+
+    Damage int unsigned NOT NULL,
     Weapon varchar(32) NOT NULL,
-    Hitgroup int NOT NULL,
-    RoundTick int NOT NULL,
+    RoundTick int unsigned NOT NULL,
 	FOREIGN KEY (RoundID) REFERENCES CS2S_Round(RoundID),
 	FOREIGN KEY (MatchID) REFERENCES CS2S_Match(MatchID),
     FOREIGN KEY (AttackerID) REFERENCES CS2S_PlayerInfo(PlayerID),
@@ -94,15 +116,19 @@ CREATE TABLE IF NOT EXISTS CS2S_Hurt (
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Blind (
-    BlindID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    RoundID int NOT NULL,
-    MatchID int NOT NULL,
-    ThrowerID bigint NOT NULL,
-	ThrowerSide int NOT NULL,
-	BlindedID bigint NOT NULL,
-	BlindedSide int NOT NULL,
+    BlindID int unsigned PRIMARY KEY AUTO_INCREMENT,
+    RoundID int unsigned NOT NULL,
+    MatchID int unsigned NOT NULL,
+
+    ThrowerID bigint unsigned NOT NULL,
+	BlindedID bigint unsigned NOT NULL,
+
+    -- t = 2, ct = 3
+    ThrowerSide tinyint unsigned NOT NULL,
+	BlindedSide tinyint unsigned NOT NULL,
+
     Duration float NOT NULL,
-    RoundTick int NOT NULL,
+    RoundTick int unsigned NOT NULL,
 	FOREIGN KEY (RoundID) REFERENCES CS2S_Round(RoundID),
 	FOREIGN KEY (MatchID) REFERENCES CS2S_Match(MatchID),
     FOREIGN KEY (ThrowerID) REFERENCES CS2S_PlayerInfo(PlayerID),
@@ -110,11 +136,15 @@ CREATE TABLE IF NOT EXISTS CS2S_Blind (
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Grenade (
-    GrenadeID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    RoundID int NOT NULL,
-    MatchID int NOT NULL,
-    ThrowerID bigint NOT NULL,
+    GrenadeID int unsigned PRIMARY KEY AUTO_INCREMENT,
+    RoundID int unsigned NOT NULL,
+    MatchID int unsigned NOT NULL,
+
+    ThrowerID bigint unsigned NOT NULL,
+
+    -- t = 2, ct = 3
 	ThrowerSide int NOT NULL,
+
     Weapon varchar(32) NOT NULL,
     RoundTick int NOT NULL,
     FOREIGN KEY (RoundID) REFERENCES CS2S_Round(RoundID),
@@ -123,23 +153,31 @@ CREATE TABLE IF NOT EXISTS CS2S_Grenade (
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_KAST (
-    KASTID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    RoundID int NOT NULL,
-    MatchID int NOT NULL,
-    PlayerID bigint NOT NULL,
-	PlayerSide int NOT NULL,
+    KASTID int unsigned PRIMARY KEY AUTO_INCREMENT,
+    RoundID int unsigned NOT NULL,
+    MatchID int unsigned NOT NULL,
+
+    PlayerID bigint unsigned NOT NULL,
+    
+    -- t = 2, ct = 3
+	PlayerSide tinyint unsigned NOT NULL,
+
     FOREIGN KEY (RoundID) REFERENCES CS2S_Round(RoundID),
 	FOREIGN KEY (MatchID) REFERENCES CS2S_Match(MatchID),
     FOREIGN KEY (PlayerID) REFERENCES CS2S_PlayerInfo(PlayerID)
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Clutch (
-    ClutchID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    RoundID int NOT NULL,
-    MatchID int NOT NULL,
-    PlayerID bigint NOT NULL,
-	PlayerSide int NOT NULL,
-    EnemyCount int NOT NULL,
+    ClutchID int PRIMARY KEY AUTO_INCREMENT,
+    RoundID int unsigned NOT NULL,
+    MatchID int unsigned NOT NULL,
+
+    PlayerID bigint unsigned NOT NULL,
+
+    -- t = 2, ct = 3
+	PlayerSide tinyint unsigned NOT NULL,
+
+    EnemyCount tinyint NOT NULL,
     Result ENUM("Win", "Loss") NOT NULL,
     FOREIGN KEY (RoundID) REFERENCES CS2S_Round(RoundID),
 	FOREIGN KEY (MatchID) REFERENCES CS2S_Match(MatchID),
@@ -147,13 +185,17 @@ CREATE TABLE IF NOT EXISTS CS2S_Clutch (
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Duel (
-    DuelID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    RoundID int NOT NULL,
-    MatchID int NOT NULL,
-    WinnerID bigint NOT NULL,
-	WinnerSide int NOT NULL,
-    LoserID bigint NOT NULL,
-    LoserSide int NOT NULL,
+    DuelID int unsigned PRIMARY KEY AUTO_INCREMENT,
+    RoundID int unsigned NOT NULL,
+    MatchID int unsigned NOT NULL,
+
+    WinnerID bigint unsigned NOT NULL,
+    LoserID bigint unsigned NOT NULL,
+
+    -- t = 2, ct = 3
+	WinnerSide smallint unsigned NOT NULL,
+    LoserSide smallint unsigned NOT NULL,
+
     FOREIGN KEY (RoundID) REFERENCES CS2S_Round(RoundID),
     FOREIGN KEY (MatchID) REFERENCES CS2S_Match(MatchID),
     FOREIGN KEY (WinnerID) REFERENCES CS2S_PlayerInfo(PlayerID),
@@ -161,49 +203,52 @@ CREATE TABLE IF NOT EXISTS CS2S_Duel (
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_LivePlayers (
-	PlayerID bigint NOT NULL,
-    Kills int DEFAULT 0 NOT NULL,
-    Assists int DEFAULT 0 NOT NULL,
-    Deaths int DEFAULT 0 NOT NULL,
+	PlayerID bigint unsigned NOT NULL,
+    Kills smallint unsigned DEFAULT 0 NOT NULL,
+    Assists smallint unsigned DEFAULT 0 NOT NULL,
+    Deaths smallint unsigned DEFAULT 0 NOT NULL,
     ADR float DEFAULT 0 NOT NULL,
-	Health int DEFAULT 0 NOT NULL, 
-    Money int DEFAULT 0 NOT NULL,
-    Side int NOT NULL
+	Health tinyint unsigned DEFAULT 0 NOT NULL, 
+    Money smallint unsigned DEFAULT 0 NOT NULL,
+
+    -- t = 2, ct = 3
+    Side tinyint unsigned NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_LiveStatus (
-	StaticID int PRIMARY KEY,
+	StaticID int unsigned PRIMARY KEY,
 	MapID varchar(128),
-    BombStatus int,
-    TScore int,
-    CTScore int,
-	InsertDate datetime DEFAULT CURRENT_TIMESTAMP
+
+    -- 0 = none (unplanted), 1 = planted, 2 = defused
+    BombStatus tinyint unsigned NOT NULL,
+
+    TScore smallint unsigned NOT NULL,
+    CTScore smallint unsigned NOT NULL,
+	InsertDate datetime DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Team_Players (
     TeamID varchar(32) NOT NULL,
-    PlayerID bigint NOT NULL,
+    PlayerID bigint unsigned NOT NULL,
     PRIMARY KEY (TeamID, PlayerID),
     FOREIGN KEY (TeamID) REFERENCES CS2S_Team(TeamID),
     FOREIGN KEY (PlayerID) REFERENCES CS2S_PlayerInfo(PlayerID)
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_Player_Matches (
-    PlayerID bigint NOT NULL,
-	MatchID int NOT NULL,
+    PlayerID bigint unsigned NOT NULL,
+	MatchID int unsigned NOT NULL,
     PRIMARY KEY (PlayerID, MatchID),
     FOREIGN KEY (PlayerID) REFERENCES CS2S_PlayerInfo(PlayerID),
     FOREIGN KEY (MatchID) REFERENCES CS2S_Match(MatchID)
 );
 
 CREATE TABLE IF NOT EXISTS CS2S_PlayerOfTheWeek (
-    PlayerID bigint NOT NULL,
-	WeekPosition int DEFAULT 0 NOT NULL,
+    PlayerID bigint unsigned NOT NULL,
+	WeekPosition int unsigned DEFAULT 0 NOT NULL,
     BaseRating float DEFAULT 0 NOT NULL,
     WeekRating float DEFAULT 0 NOT NULL,
     RatingDelta float DEFAULT 0 NOT NULL,
     PRIMARY KEY (PlayerID),
     FOREIGN KEY (PlayerID) REFERENCES CS2S_PlayerInfo(PlayerID)
 );
-
-
